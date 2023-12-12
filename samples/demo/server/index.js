d7a116a0f7c274e0bf863beec559badd837b6a70
wangxiang <forhot2000@qq.com>
2023-12-12 19:21:18 +0800

    extract functions

--- samples/demo/server/index.js
+++ samples/demo/server/index.js
@@ -1,6 +1,6 @@
-import { createServer as createViteServer } from "vite";
-import httpProxy from "http-proxy";
 import express from "express";
+import httpProxy from "http-proxy";
+import { createServer as createViteServer } from "vite";
 
 const MIME = { javascript: "application/javascript" };
 
@@ -25,31 +25,25 @@ function slowRequest(delay, handler) {
   };
 }
 
+const testJs = slowRequest(1000, function (req, res) {
+  const { id = "1" } = req.query;
+  res.contentType(MIME.javascript).send(`
+    (function () {
+      const id = "${id}";
+      console.log(id);
+      document.addEventListener("DOMContentLoaded", function () {
+        console.log(id + "-ready");
+      });
+    })();
+  `);
+});
+
 async function createServer() {
   const app = express();
 
-  const vite = await createViteServer({
-    configFile: "vite.config.js",
-    server: {
-      middlewareMode: true,
-    },
-  });
+  const vite = await setupVite();
 
-  app.use(
-    "/test.js",
-    slowRequest(1000, function (req, res) {
-      const { id = "1" } = req.query;
-      res.contentType(MIME.javascript).send(`
-        (function () {
-          const id = "${id}";
-          console.log(id);
-          document.addEventListener("DOMContentLoaded", function () {
-            console.log(id + "-ready");
-          });
-        })();
-      `);
-    })
-  );
+  app.use("/test.js", testJs);
 
   app.use(express.static("public"));
 
@@ -57,6 +51,15 @@ async function createServer() {
 
   app.listen(3000, function () {
     console.log("app listen on http://localhost:3000");
+  });
+}
+
+async function setupVite() {
+  return await createViteServer({
+    configFile: "vite.config.js",
+    server: {
+      middlewareMode: true,
+    },
   });
 }
 
